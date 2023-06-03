8c7d574ea44a144e27e3c3f75164fed538ea0240
Laura Wu <leemaiwu@gmail.com>
2023-06-03 14:54:57 -0600

    passed 103 failed 1

--- solution.js
+++ solution.js
@@ -1,3 +1,32 @@
-const isValidWalk = (walk) => {
-    walk.length == 10 ? true : false
+const isValidWalk = walk => {
+    let n = 0
+    let w = 0
+    let s = 0
+    let e = 0
+    for (let i = 0; i < walk.length; i++) {
+        if (walk[i] === 'n') {
+            n++
+        } else if (walk[i] === 'w') {
+            w++
+        } else if (walk[i] === 's') {
+            s++
+        } else if (walk[i] === 'e') {
+            e++
+        } else {
+            return false
+        }
+    }
+    let count = [n, s, w, e]
+    console.log(count)
+    if (walk.length === 10 && ((count[0] === count[1] && count[1] > 0) || (count[2] === count[3] && count[3] > 0 ))) {
+        return true
+    } else {
+        return false
+    }
 }
+
+console.log(isValidWalk(['w'])) // false
+console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s'])) // false
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s'])) // true
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n'])) // false
+console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s'])) // false
